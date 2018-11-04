dd55003249086667767c87d98aa28643f83ac8a6
umanshi <b.umanshi@gmail.com>
2018-11-05 05:22:39 +0530

    Update equal.cpp

--- Hashing/equal.cpp
+++ Hashing/equal.cpp
@@ -9,31 +9,30 @@ vector<int> Solution::equal(vector<int> &A) {
                 m[sum].push_back(i);
                 m[sum].push_back(j); 
             }
-            else{ //found
-                if(m[sum][0]!=i && m[sum][1]!=j && m[sum][1]!=i){
-                    vector<int> temp_ans;
-                    temp_ans.push_back(m[sum][0]);
-                    temp_ans.push_back(m[sum][1]);
-                    temp_ans.push_back(i);
-                    temp_ans.push_back(j);
-                    if(temp_ans[0]<ans[0]){ //cout<<"Case 1"<<endl;
+            else if(m[sum][0]!=i && m[sum][1]!=j && m[sum][1]!=i){ // found
+                vector<int> temp_ans;
+                temp_ans.push_back(m[sum][0]);
+                temp_ans.push_back(m[sum][1]);
+                temp_ans.push_back(i);
+                temp_ans.push_back(j);
+                if(temp_ans[0]<ans[0]){ //cout<<"Case 1"<<endl;
+                    ans.swap(temp_ans); }
+                else if(temp_ans[0]==ans[0]){ //cout<<"Case 2"<<endl;
+                    if(temp_ans[1]<ans[1]){
                         ans.swap(temp_ans); }
-                    else if(temp_ans[0]==ans[0]){ //cout<<"Case 2"<<endl;
-                        if(temp_ans[1]<ans[1]){
-                            ans.swap(temp_ans); }
-                        else if(temp_ans[1]==ans[1]){ //cout<<"Case 3"<<endl;
-                            if(temp_ans[2]<ans[2]){
-                                ans.swap(temp_ans);}  
-                            else if(temp_ans[2]==ans[2]){ //cout<<"Case 4"<<endl;
-                                if(temp_ans[3]<ans[3])
-                                    {ans.swap(temp_ans);}  
-                            }
+                    else if(temp_ans[1]==ans[1]){ //cout<<"Case 3"<<endl;
+                        if(temp_ans[2]<ans[2]){
+                            ans.swap(temp_ans);}  
+                        else if(temp_ans[2]==ans[2]){ //cout<<"Case 4"<<endl;
+                            if(temp_ans[3]<ans[3])
+                                {ans.swap(temp_ans);}  
                         }
-                    }    
-                }
+                    }
+                }    
             }
         }
     }
+
     if(ans[0]!=-1)
         return ans;
     vector<int> a;
